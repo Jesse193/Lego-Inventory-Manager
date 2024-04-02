7af40a47fc0edd9f90d7a66c044f90e8ef13f7f0
Jesse <jessesorman@gmail.com>
2024-04-02 01:02:19 -0400

    refactor part color mode into one file

--- Lego Inventory Manager/Models/RootObject.cs
+++ Lego Inventory Manager/Models/RootObject.cs
@@ -8,4 +8,19 @@ namespace LegoInventoryManager.Models
         public List<Result> result { get; set; }
         public string PartNumber {get; set;}
     }
+
+    public class Result
+    {
+        [JsonPropertyName("color_name")]
+        public string Color { get; set; }
+
+        [JsonPropertyName("part_img_url")]
+        public string Image { get; set; }
+
+        [JsonPropertyName("color_id")]
+        public int ColorId { get; set; }
+
+        [JsonPropertyName("elements")]
+        public List<string> ElementList { get; set; }
+    }
 }

e5969ef897457001aeebc7745da6c3c9dccc3158
Jesse <jessesorman@gmail.com>
2024-04-01 21:31:22 -0400

    Adds feature to add part to collection

---
+++ Lego Inventory Manager/Controllers/PartLists.cs
@@ -0,0 +1,24 @@
+﻿using LegoInventoryManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LegoInventoryManager.Controllers
+{
+	public class PartLists : Controller
+	{
+		private readonly ILegoApiService _legoApiService;
+
+		public PartLists(ILegoApiService legoApiService)
+		{
+			_legoApiService = legoApiService;
+		}
+
+		public async Task<IActionResult> Post(string userToken, string listId, string partNumber, int Quantity, int colorId)
+		{
+			PartList myParts = new PartList();
+			myParts = await _legoApiService.AddPartsToList(userToken, listId, partNumber, Quantity, colorId);
+
+			return View(myParts);
+		}
+	}
+}

d780ac98bd77ce5a46029ce278a03c29a9f0b45a
Jesse <jessesorman@gmail.com>
2024-04-07 18:23:56 -0400

    Add SetListSet model

---
+++ Lego Inventory Manager/Models/SetListSet.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace LegoInventoryManager.Models
+{
+    public class SetListSet
+    {
+        [JsonPropertyName("set_num")]
+        public string SetNumber { get; set; }
+
+        [JsonPropertyName("quantity")]
+        public int Quantity { get; set; }
+
+        [JsonPropertyName("include_spares")]
+        public bool IncludeSpares { get; set; }
+    }
+}
+/*
+ * Post api/v3/users/userToken/setlists/1025289/sets/
+{
+    "set_num": "75333-1",
+  "quantity": 1,
+  "include_spares": false
+}
+*/

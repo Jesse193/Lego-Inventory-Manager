192728f5d9e2fde085fd3f1f6f38d1afc207d042
Jesse <jessesorman@gmail.com>
2024-04-07 17:39:28 -0400

    Add SetListsController and Post action

---
+++ Lego Inventory Manager/Controllers/SetListsController.cs
@@ -0,0 +1,22 @@
+﻿using LegoInventoryManager.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LegoInventoryManager.Controllers
+{
+    public class SetListsController : Controller
+    {
+        private readonly ILegoApiService _legoApiService;
+
+        public SetListsController(ILegoApiService legoApiService)
+        {
+            _legoApiService = legoApiService;
+        }
+        public async Task<IActionResult> Post(string userToken, string Name)
+        {
+            SetList newList = new SetList();
+            newList = await _legoApiService.CreateNewSetList(userToken, Name);
+
+            return View(newList);
+        }
+    }
+}

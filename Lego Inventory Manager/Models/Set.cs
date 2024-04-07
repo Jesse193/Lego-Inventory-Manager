45df6fde9f28b71770bcfaa7a36f9714d2eebe19
Jesse <jessesorman@gmail.com>
2024-04-07 00:13:35 -0400

    Add classes to Set model

--- Lego Inventory Manager/Models/Set.cs
+++ Lego Inventory Manager/Models/Set.cs
@@ -4,6 +4,25 @@ namespace LegoInventoryManager.Models
 {
     public class Set
     {
-        
+        [JsonPropertyName("results")]
+        public List<SetResults> Results { get; set; }
+    }
+
+    public class SetResults
+    {
+        [JsonPropertyName("set_num")]
+        public string SetNum { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("year")]
+        public int Year { get; set; }
+
+        [JsonPropertyName("num_parts")]
+        public int NumParts { get; set; }
+
+        [JsonPropertyName("set_img_url")]
+        public string Image { get; set; }
     }
 }

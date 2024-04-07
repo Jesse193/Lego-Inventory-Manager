d0827a4691a9e7a3b02684c7773995a4d4b35b8f
Jesse <jessesorman@gmail.com>
2024-04-06 23:56:23 -0400

    Add Sets controller

---
+++ Lego Inventory Manager/Controllers/SetsController.cs
@@ -0,0 +1,23 @@
+﻿using LegoInventoryManager.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LegoInventoryManager.Controllers
+{
+    public class SetsController : Controller
+    {
+        private readonly ILegoApiService _legoApiService;
+
+        public SetsController(ILegoApiService legoApiService)
+        {
+            _legoApiService = legoApiService;
+        }
+
+        public async Task<IActionResult> Index(string userToken)
+        {
+            Set getSet = new Set();
+            getSet = await _legoApiService.GetSet(userToken);
+
+            return View(getSet);
+        }
+    }
+}

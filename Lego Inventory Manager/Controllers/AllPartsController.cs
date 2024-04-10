c18748e160370027c710fa9565e04751887b5841
Jesse <jessesorman@gmail.com>
2024-04-10 18:41:26 -0400

    Add AllPartsController

---
+++ Lego Inventory Manager/Controllers/AllPartsController.cs
@@ -0,0 +1,24 @@
+﻿using LegoInventoryManager.Models;
+using LegoInventoryManager.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LegoInventoryManager.Controllers
+{
+    public class AllPartsController : Controller
+    {
+        private readonly ILegoApiService _legoApiService;
+
+        public AllPartsController(ILegoApiService legoApiService)
+        {
+            _legoApiService = legoApiService;
+        }
+
+        public async Task<IActionResult> Index(string userToken)
+        {
+            AllPart allParts = new AllPart();
+            allParts = await _legoApiService.GetParts(userToken);
+
+            return View(allParts);
+        }
+    }
+}

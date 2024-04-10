04544085df699527be26f2e49e8e1c1dc7f8f0d6
Jesse <jessesorman@gmail.com>
2024-04-10 18:51:07 -0400

    Add AllPart model

---
+++ Lego Inventory Manager/Models/AllPart.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+
+namespace LegoInventoryManager.Models
+{
+    public class AllPart
+    {
+        [JsonPropertyName("count")]
+        int Count { get; set; }
+
+        [JsonPropertyName("next")]
+        public string Next { get; set; }
+
+        [JsonPropertyName("previous")]
+        public string Previous { get; set; }
+
+        [JsonPropertyName("results")]
+        public List<AllPartDetail> Results { get; set; }
+    }
+
+    public class AllPartDetail
+    {
+        [JsonPropertyName("quantity")]
+        public int Quantity { get; set; }
+
+        [JsonPropertyName("part")]
+        public PartResults Part { get; set; }
+
+        [JsonPropertyName("color")]
+        public Color Color { get; set; }
+    }
+}

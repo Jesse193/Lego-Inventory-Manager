9bcf5d711a9720658a27edb6d9c03d082951d2d7
Jesse <jessesorman@gmail.com>
2024-04-13 20:36:04 -0400

    Add previous page button to part lists view

--- Lego Inventory Manager/Models/PartList.cs
+++ Lego Inventory Manager/Models/PartList.cs
@@ -18,6 +18,9 @@ namespace LegoInventoryManager.Models
 
         [JsonPropertyName("next")]
         public string Next { get; set; }
+
+        [JsonPropertyName("previous")]
+        public string Previous { get; set; }
     }
 
     public class Results
